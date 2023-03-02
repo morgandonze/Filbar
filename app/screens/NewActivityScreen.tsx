56e6a1f13b128dde1bb08ad945c61da17719346a
Morgan Donze <donzemorgan@gmail.com>
2023-03-02 11:51:11 -0500

    Add datetime fields to NewActivityScreen

--- app/screens/NewActivityScreen.tsx
+++ app/screens/NewActivityScreen.tsx
@@ -6,6 +6,8 @@ import { TextInput, View } from "react-native";
 import { Button, Header, Text } from "../components";
 import { AppStackParamList } from "../navigators";
 import { ProjectListScreen } from "./ProjectListScreen";
+import React, { useState } from 'react'
+import DatePicker from 'react-native-date-picker'
 
 
 type NavProp = StackNavigationProp<AppStackParamList, "NewActivity">
@@ -13,6 +15,9 @@ type NavProp = StackNavigationProp<AppStackParamList, "NewActivity">
 export const NewActivityScreen: FC = observer(function NewActivityScreen(props: any) {
     const navigation: NavProp = useNavigation<NavProp>()
     const { params: { id: projectId, color, title } } = props.route
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
     // navigation.navigate("ProjectList")
 
     const saveActivity = () => {
@@ -31,21 +36,35 @@ export const NewActivityScreen: FC = observer(function NewActivityScreen(props: 
                 }}
             />
 
-            <Text>Start Time</Text>
-            <TextInput
-                style={styles.input}
-                value={null}
-                placeholder="Start Time"
-                onChangeText={() => (null)}
+            <Button text="Pick Start Date" onPress={() => setOpen(true)} />
+            <DatePicker
+                modal
+                open={open}
+                date={startDate}
+                onConfirm={(date) => {
+                    setOpen(false)
+                    setStartDate(date)
+                }}
+                onCancel={() => {
+                    setOpen(false)
+                }}
             />
+            <Text>{`${startDate}`}</Text>
 
-            <Text>End Time</Text>
-            <TextInput
-                style={styles.input}
-                value={null}
-                placeholder="End Time"
-                onChangeText={() => (null)}
+            <Button text="Pick End Date" onPress={() => setOpen(true)} />
+            <DatePicker
+                modal
+                open={open}
+                date={endDate}
+                onConfirm={(date) => {
+                    setOpen(false)
+                    setEndDate(date)
+                }}
+                onCancel={() => {
+                    setOpen(false)
+                }}
             />
+            <Text>{`${endDate}`}</Text>
 
             <Text>Efficiency</Text>
             <TextInput
@@ -55,19 +74,19 @@ export const NewActivityScreen: FC = observer(function NewActivityScreen(props: 
                 onChangeText={() => (null)}
             />
 
-        <Button
-          text="Save"
-          onPress={saveActivity}
-        />
+            <Button
+                text="Save"
+                onPress={saveActivity}
+            />
         </View>
     )
 })
 
 const styles = {
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
 }
