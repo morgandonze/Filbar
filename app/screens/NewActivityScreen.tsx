29674e714d1793103e468b40f3316f90f6e9b4ea
Morgan Donze <donzemorgan@gmail.com>
2023-03-22 02:15:35 -0400

    Activity size buttons. Hide date, efficiency.

--- app/screens/NewActivityScreen.tsx
+++ app/screens/NewActivityScreen.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { observer } from "mobx-react-lite";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { TextInput, View } from "react-native";
 import { Button, Header, Text } from "../components";
 import { AppStackParamList } from "../navigators";
@@ -9,45 +9,41 @@ import { ProjectListScreen } from "./ProjectListScreen";
 import React, { useState } from 'react'
 import DatePicker from 'react-native-date-picker'
 import { useStores } from "../models";
+import { FlatList } from "react-native-gesture-handler";
 
 
 type NavProp = StackNavigationProp<AppStackParamList, "NewActivity">
 
 export const NewActivityScreen: FC = observer(function NewActivityScreen() {
     const navigation: NavProp = useNavigation<NavProp>()
-    // const { params: activityParams } = props.route
-
     const rootStore = useStores();
-    const { currentProject: currentProjectId } = rootStore;
-    const project = rootStore.getProjectById(currentProjectId);
-    const { id: projectId, color, title } = project;
 
+    const [showDatePickers, setShowDatePickers] = useState(false)
     const [startOpen, setStartOpen] = useState(false)
     const [endOpen, setEndOpen] = useState(false)
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
     const [value, setValue] = useState(1);
+    // useEffect(() => { }, [value])
 
-
-    const saveActivity = () => {
-        return () => {
-            rootStore.addActivity(Date.now(), value, projectId);
-            navigation.goBack();
-        }
+    const project = rootStore.getProjectById(rootStore.currentProject);
+    const { id: projectId, color, title } = project;
+    const sizeValues = {
+        "XS": 1,
+        "S": 2,
+        "M": 3,
+        "L": 5,
+        "XL": 8,
+        "XXL": 13,
     }
 
-    return (
-        <View >
-            <Header
-                title={`New Activity for ${title}`}
-                titleStyle={{ color: "white" }}
-                style={{ backgroundColor: color, marginBottom: 10 }}
-                leftIcon="back"
-                onLeftPress={() => {
-                    navigation.goBack()
-                }}
-            />
+    const saveActivity = () => {
+        rootStore.addActivity(Date.now(), value, projectId);
+        navigation.goBack();
+    }
 
+    const renderDatePickers = () => (
+        <>
             <Button text="Pick Start Date" onPress={() => setStartOpen(true)} />
             <DatePicker
                 modal
@@ -77,18 +73,47 @@ export const NewActivityScreen: FC = observer(function NewActivityScreen() {
                 }}
             />
             <Text>{`${endDate}`}</Text>
+        </>
+    )
 
-            <Text>Efficiency</Text>
-            <TextInput
-                style={styles.input}
-                value={null}
-                placeholder="Efficiency"
-                onChangeText={() => (null)}
+    const renderActivitySizes = () => (
+        <View style={{ marginBottom: 40 }}>
+            <FlatList
+                data={Object.entries(sizeValues)}
+                contentContainerStyle={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}
+                renderItem={({ item }) => {
+                    const size = item[0]
+                    const thisValue = item[1]
+                    return <Button text={size} onPress={() => {
+                        setValue(thisValue)
+                    }} style={{
+                        flex: 1,
+                        backgroundColor: value == thisValue ? project.color : '#fff'
+                    }} />
+                }}
             />
+        </View>
+    )
+
+    return (
+        <View >
+            <Header
+                title={`New Activity for ${title}`}
+                titleStyle={{ color: "white" }}
+                style={{ backgroundColor: color, marginBottom: 40 }}
+                leftIcon="back"
+                onLeftPress={() => {
+                    navigation.goBack()
+                }}
+            />
+
+            {showDatePickers && renderDatePickers()}
+
+            {renderActivitySizes()}
 
             <Button
                 text="Save"
-                onPress={saveActivity()}
+                onPress={saveActivity}
             />
         </View>
     )
