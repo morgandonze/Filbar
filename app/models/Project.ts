493462e06a0bfcda69ad11d9ab61637022980741
Morgan Donze <donzemorgan@gmail.com>
2023-03-30 16:32:57 -0400

    Add Activity actions to Project

--- app/models/Project.ts
+++ app/models/Project.ts
@@ -1,11 +1,11 @@
 import { types } from "mobx-state-tree";
-import { identifier } from "mobx-state-tree/dist/internal";
+import { identifier, Instance } from "mobx-state-tree/dist/internal";
 import { Activity } from "./Activity";
 
 export const Project = types
     .model({
         id: types.refinement(types.identifier, identifier => identifier.indexOf("Project_") === 0),
-        activities: types.optional(types.array(Activity), []),
+        activities: types.optional(types.array(types.reference(Activity)), []),
         title: types.optional(types.string, "New Project"),
         color: types.string,
         priority: types.optional(types.integer, 0),
@@ -14,9 +14,20 @@ export const Project = types
     .views(self => ({
         get velocity() {
             return 100;
+        },
+        get activityCount() {
+            console.log("activities", self.activities)
+            return self.activities.length;
         }
     }))
     .actions(self => ({
+        addActivity(activity: Instance<typeof Activity>) {
+            self.activities.push(activity);
+        },
+        removeActivity(targetActivity: Instance<typeof Activity>) {
+            const index = self.activities.findIndex(activity => (activity.id == targetActivity.id));
+            if (index >= 0) self.activities.splice(index, 1);
+        },
         updateTite(title) {
             self.title = title;
         },
