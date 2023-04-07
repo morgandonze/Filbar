750a47cea8183e7f2f87738959fb75a1f27ecbd2
Morgan Donze <donzemorgan@gmail.com>
2023-04-07 03:07:25 -0400

    Update colors

--- app/theme/projectColors.ts
+++ app/theme/projectColors.ts
@@ -2,11 +2,11 @@ const projectColors = {
     purple: "#9d5ad6",
     snowbolBlue2: "#08aad8",
     rose4: "#e02415",
-    teal: "#3d68b7",
     grass2: "#a6cb35",
-    o: "#d98314",
+    o: "#EA8534",
     g: "#81920d",
-    goldenrod: "#daa520",
+    goldenrod: "#f6ba2c",
+    bloo: "#2781f3",
 }
 export {
     projectColors
